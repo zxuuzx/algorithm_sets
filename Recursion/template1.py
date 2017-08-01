8eab2a16e98613e181d840923510c57ca56353d9
Xin <zxuuzx@gmail.com>
2017-08-01 12:53:21 -0500

    Add Recursion dir and template

---
+++ Recursion/template1.py
@@ -0,0 +1,11 @@
+"""Example function"""
+def recursion_function(num):
+    """Recursively print"""
+    # ending condition
+    if num == 0:
+        return
+    print num
+    # call itself
+    recursion_function(num - 1)
+
+recursion_function(5)
